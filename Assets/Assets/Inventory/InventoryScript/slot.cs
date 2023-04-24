5e2a0f8f83964b9b03843b40a086af9283ff4964
fiona <fiona.w32@gmail.com>
2023-04-24 21:59:33 +0800

    merge看看

--- Assets/Assets/Inventory/InventoryScript/slot.cs
+++ Assets/Assets/Inventory/InventoryScript/slot.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 public class slot : MonoBehaviour
 {
+    //命名為item類才能取得
    public item slotItem;
     public Image slotImage;
     //public int slotNum;
+    public void ItemOnclicked(){
+        InventoryManager.UpdateItemInfo(slotItem.itemInfo);
+    }
 }
