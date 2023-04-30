102aff1f8eaff6e4bd210fd6ff63e257bc4d120d
pinyi <s110919041@stu.ntue.edu.tw>
2023-05-01 00:48:40 +0800

    新增手冊icon/各場景物件調整/麝黜對話完成

--- Assets/Scripts/ShowCanvasOnClick.cs
+++ Assets/Scripts/ShowCanvasOnClick.cs
@@ -3,34 +3,7 @@ using UnityEngine.UI;
 
 public class ShowCanvasOnClick : MonoBehaviour
 {
-    // public GameObject canvas;
 
-    // void Start()
-    // {
-    //     // 隐藏 Canvas
-    //     canvas.SetActive(false);
-    // }
-
-    // void Update()
-    // {
-    //     // 如果点击了鼠标左键
-    //     if (Input.GetMouseButtonDown(0))
-    //     {
-    //         // 获取鼠标的位置
-    //         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-    //         // 获取被点击的物体的位置
-    //         Vector2 objPos = transform.position;
-
-    //         // 如果鼠标点击了这个物体
-    //         if (mousePos.x >= objPos.x - 0.5f && mousePos.x <= objPos.x + 0.5f
-    //             && mousePos.y >= objPos.y - 0.5f && mousePos.y <= objPos.y + 0.5f)
-    //         {
-    //             // 显示 Canvas
-    //             canvas.SetActive(true);
-    //         }
-    //     }
-    // }
  public GameObject canvasToToggle;
 
     private bool canvasIsActive = false;
