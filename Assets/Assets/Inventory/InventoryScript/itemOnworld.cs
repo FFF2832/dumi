926a20662eb247c6446ae1f05415b7aaee83edf5
pinyi <s110919041@stu.ntue.edu.tw>
2023-05-11 01:20:27 +0800

    目前一切正常(拜託給我保持)

--- Assets/Assets/Inventory/InventoryScript/itemOnworld.cs
+++ Assets/Assets/Inventory/InventoryScript/itemOnworld.cs
@@ -1,13 +1,108 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class itemOnworld : MonoBehaviour
+// {
+//    public static item thisItem;
+//     // public static slot thisSlot;
+//    public Inventory playerInventory;
+// public static int id;
+//   private void Update(){
+//     check2DObjectClicked();
+//   }
+//    private void OnTriggerEnter2D(Collider2D other)
+//    {
+//     if(other.gameObject.CompareTag("Player")){
+//         AddNewItem();
+//         Destroy(gameObject);
+//         Debug.Log("touch");
+//     }  
+//    } 
+//    public void AddNewItem(){
+//     if(!playerInventory.itemList.Contains(thisItem)){
+//          //playerInventory.itemList.Add(thisItem);
+//           //未刪CreateNewItem
+//          //InventoryManager.CreateNewItem(thisItem);
+//          for(int i=0;i<playerInventory.itemList.Count;i++){
+//                 if(playerInventory.itemList[i]==null){
+//                         playerInventory.itemList[i]=thisItem;
+//                         Debug.Log(thisItem.name);
+//                         break;
+//                 }
+//          }
+//     }
+//     else {
+//         thisItem.itemHeild += 1;
+//     }
+//     InventoryManager.RefreshItem(); 
+//    }
+   
+    
+//    void check2DObjectClicked()
+// {
+//     if (Input.GetMouseButtonDown(0))
+//     {
+//         Debug.Log("Mouse is pressed down");
+//         Camera cam = Camera.main;
+
+//         //Raycast depends on camera projection mode
+//         Vector2 origin = Vector2.zero;
+//         Vector2 dir = Vector2.zero;
+
+//         if (cam.orthographic)
+//         {
+//             origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//         }
+//         else
+//         {
+//             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//             origin = ray.origin;
+//             dir = ray.direction;
+//         }
+
+//         RaycastHit2D hit = Physics2D.Raycast(origin, dir);
+
+//         //Check if we hit anything
+//         if (hit)
+//         {
+            
+          
+//             if(hit.collider.name=="branch"){
+             
+//                 Debug.Log("We hit " + hit.collider.name);
+//                 AddNewItem();
+//                 Destroy(hit.collider);
+//                  Destroy(gameObject);
+//                 Debug.Log("touch");
+               
+//             }
+//             if(hit.collider.name=="零件2"){
+             
+//                 Debug.Log("We hit " + hit.collider.name);
+//                 AddNewItem();
+//                 Destroy(gameObject);
+//                 Destroy(hit.collider);
+//                 Debug.Log("touch零件");
+//             }
+             
+
+//         }
+//     } 
+    
+// }
+
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class itemOnworld : MonoBehaviour
 {
-   public static item thisItem;
-    // public static slot thisSlot;
+   public item thisItem;
    public Inventory playerInventory;
-public static int id;
+
   private void Update(){
     check2DObjectClicked();
   }
@@ -27,7 +122,6 @@ public static int id;
          for(int i=0;i<playerInventory.itemList.Count;i++){
                 if(playerInventory.itemList[i]==null){
                         playerInventory.itemList[i]=thisItem;
-                        Debug.Log(thisItem.name);
                         break;
                 }
          }
@@ -37,8 +131,7 @@ public static int id;
     }
     InventoryManager.RefreshItem(); 
    }
-   
-    
+
    void check2DObjectClicked()
 {
     if (Input.GetMouseButtonDown(0))
@@ -68,16 +161,15 @@ public static int id;
         {
             
           
-            if(hit.collider.name=="branch"){
+            if(hit.collider.name=="樹枝本人"){
              
                 Debug.Log("We hit " + hit.collider.name);
                 AddNewItem();
                 Destroy(hit.collider);
                  Destroy(gameObject);
                 Debug.Log("touch");
-               
             }
-            if(hit.collider.name=="零件2"){
+            if(hit.collider.name=="零件"){
              
                 Debug.Log("We hit " + hit.collider.name);
                 AddNewItem();
