e85180ae547a635505dc369243986b7d5cfa50ea
pinyi <s110919041@stu.ntue.edu.tw>
2023-06-23 15:55:06 +0800

    增加阿竹動畫 / 修正畫返回正確場景

---
+++ Assets/Scripts/Scene3Script.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene3Script : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("PreviousScene", 3);
+    }
+    // 其他代码
+
+}
