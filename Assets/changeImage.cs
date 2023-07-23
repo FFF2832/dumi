16920dee5a3e4135e030c685e49c2c66a89b63c0
fiona <fiona.w32@gmail.com>
2023-07-23 16:08:52 +0800

    又是失敗的一天

--- Assets/changeImage.cs
+++ Assets/changeImage.cs
@@ -93,64 +93,106 @@ public class changeImage : MonoBehaviour
 // }
 
 
-///最開始的版本(同時換圖)
-    // void Update()
-    // {
+// // /最開始的版本(同時換圖)
+//     void Update()
+//     {
       
-    //     //撿樹枝剪零件2
-    //     if(ItemOndrag.checkPosition()==1&&ItemOndrag.checkitemPosition()==1){
-    //           AddNewItem();
-    //           //Destroy(gameObject);
-    //            spriteChange.sprite = sprite1;
-    //            RemoveItem(itemToRemove);
-    //     }
-    //     //撿零件1到太陽位置
-    //    else if(ItemOndrag.checkPosition()==2&&ItemOndrag.checkitemPosition()==2){
-    //           spriteChange.sprite = sprite4;
-    //              RemoveItem(itemToRemove);
-    //     }
-    //      //撿零件2到圓形位置
-    //     else if(ItemOndrag.checkPosition()==3&&ItemOndrag.checkitemPosition()==3){
-    //           spriteChange.sprite = sprite3;
-    //              RemoveItem(itemToRemove);
-    //     }
+//         //撿樹枝剪零件2
+//         if(ItemOndrag.checkPosition()==1&&ItemOndrag.checkitemPosition()==1){
+//               AddNewItem();
+//               //Destroy(gameObject);
+//                spriteChange.sprite = sprite1;
+//                RemoveItem(itemToRemove);
+//         }
+//         //撿零件1到太陽位置
+//        else if(ItemOndrag.checkPosition()==2&&ItemOndrag.checkitemPosition()==2){
+//               spriteChange.sprite = sprite4;
+//                  RemoveItem(itemToRemove);
+//         }
+//          //撿零件2到圓形位置
+//         else if(ItemOndrag.checkPosition()==3&&ItemOndrag.checkitemPosition()==3){
+//               spriteChange.sprite = sprite3;
+//                  RemoveItem(itemToRemove);
+//         }
         
-    //     else
-    //     {
-    //         spriteChange.sprite = sprite2;
-    //     }
-    // }
+//         else
+//         {
+//             spriteChange.sprite = sprite2;
+//         }
+//     }
 
-    void Update()
+//     void Update()
+// {
+//     if (gameObject == this.gameObject)
+//     {
+//         // 檢查拖曳的物品是否是樹枝或剪刀
+//         int position = ItemOndrag.checkPosition();
+//         int itemPosition = ItemOndrag.checkitemPosition();
+
+//         if (position == 1 && itemPosition == 1)
+//         {
+//             AddNewItem(thisItem);
+//             spriteChange.sprite = sprite1;
+//             RemoveItem(itemToRemove);
+//         }
+//         else if (position == 2 && itemPosition == 2)
+//         {
+//             spriteChange.sprite = sprite4;
+//             RemoveItem(itemToRemove);
+//         }
+//         else if (position == 3 && itemPosition == 3)
+//         {
+//             spriteChange.sprite = sprite3;
+//             RemoveItem(itemToRemove);
+//         }
+//         else
+//         {
+//             spriteChange.sprite = sprite2;
+//         }
+//     }
+// }
+
+
+
+// 只會換一邊
+void Update()
 {
     if (gameObject == this.gameObject)
     {
         // 檢查拖曳的物品是否是樹枝或剪刀
         int position = ItemOndrag.checkPosition();
         int itemPosition = ItemOndrag.checkitemPosition();
-
-        if (position == 1 && itemPosition == 1)
+        Debug.Log("itemPosition"+itemPosition);
+        // 檢查是否在正確的位置上
+        if (ItemOndrag.checkTarget())
         {
-            AddNewItem(thisItem);
-            spriteChange.sprite = sprite1;
-            RemoveItem(itemToRemove);
-        }
-        else if (position == 2 && itemPosition == 2)
-        {
-            spriteChange.sprite = sprite4;
-            RemoveItem(itemToRemove);
-        }
-        else if (position == 3 && itemPosition == 3)
-        {
-            spriteChange.sprite = sprite3;
-            RemoveItem(itemToRemove);
+            if (position == 1 && itemPosition == 1)
+            {
+                // 在正確的位置上且拖曳的物品是樹枝，更換成 sprite1
+                AddNewItem();
+                spriteChange.sprite = sprite1;
+                RemoveItem(itemToRemove);
+                
+            }
+            else if (position == 2 && itemPosition == 2)
+            {
+                // 在正確的位置上且拖曳的物品是零件1，更換成 sprite4
+                spriteChange.sprite = sprite4;
+            }
+            else if (position == 3 && itemPosition == 3)
+            {
+                // 在正確的位置上且拖曳的物品是零件2，更換成 sprite3
+                spriteChange.sprite = sprite3;
+            }
         }
         else
         {
+            // 如果不在正確的位置上，顯示 sprite2
             spriteChange.sprite = sprite2;
         }
     }
 }
+
 
   public static int GetTargetInfo(GameObject gameObject)
 {
@@ -167,7 +209,7 @@ public class changeImage : MonoBehaviour
         return 0; // or any other appropriate value
     }
 }
-public void AddNewItem(item thisItem){
+public void AddNewItem(){
     if(!playerInventory.itemList.Contains(thisItem)){
          //playerInventory.itemList.Add(thisItem);
           //未刪CreateNewItem
