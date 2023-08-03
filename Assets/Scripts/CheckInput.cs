dc5d7589c4ec7a024d8a94af0c44f993fde95814
fiona <fiona.w32@gmail.com>
2023-08-04 00:20:29 +0800

    換圖成功(remove還沒修)

--- Assets/Scripts/CheckInput.cs
+++ Assets/Scripts/CheckInput.cs
@@ -7,7 +7,10 @@ public class CheckInput : MonoBehaviour
 {
      public Sprite sprite1; // 第一個圖片
     public Sprite sprite2; // 第二個圖片
+     public Sprite sprite3; // 第二個圖片
+     public Sprite sprite4; // 第二個圖片
      private SpriteRenderer spriteChange;
+   
    
     public static bool ChangeScene ;
     void Start(){
@@ -18,20 +21,43 @@ public class CheckInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PassWord.checkInput()&&ItemOndrag.checkTarget()) // 呼叫 PassWord 的 checkInput 方法，回傳 bool 值
-        {
-            spriteChange.sprite = sprite2; // 切換成第一個圖片
+        if(!PassWord.checkInput()){
+            if (ItemOndrag.checktire1())
+            {
+        // 在正確的位置上且拖曳的物品輪胎1，更換成 sprite1
+                spriteChange.sprite = sprite1;
+            }
+            if (ItemOndrag.checktire2())
+            {
+        // 在正確的位置上且拖曳的物品是輪胎2，更換成 sprite2
+                spriteChange.sprite = sprite2;
+            }
+            if(ItemOndrag.checktire1()&&ItemOndrag.checktire2()){
+                spriteChange.sprite = sprite3; // 切換好車
+            }
+            else
+            {
+            //壞車，顯示 sprite4
+                spriteChange.sprite = sprite4;
+            }
+        }
+
+       else{
+            if (PassWord.checkInput()&&ItemOndrag.checktire1()&&ItemOndrag.checktire2()) // 呼叫 PassWord 的 checkInput 方法，回傳 bool 值
+            {
+            spriteChange.sprite = sprite3; // 切換好車
            
             ChangeScene=true;
             
-        }
+            }
        
-        else
-        {
-            spriteChange.sprite = sprite1; // 切換成第二個圖片
+            else
+            {
+            spriteChange.sprite = sprite4; // 切換成第二個圖片
         
             ChangeScene=false;
-        }
+            }
+       }
 
     }
     public static void ChangeTonight(){
