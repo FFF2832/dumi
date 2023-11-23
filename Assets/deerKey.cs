349dc3ab26d204831b1ca9cec97bafff96914f5f
fiona <fiona.w32@gmail.com>
2023-11-23 17:16:45 +0800

    路頭

--- Assets/deerKey.cs
+++ Assets/deerKey.cs
@@ -13,6 +13,8 @@ public class deerKey : MonoBehaviour
     public GameObject obj1;
       public GameObject obj2;
        public GameObject obj3;
+        public GameObject obj4;
+         public GameObject obj5;
 
     void Start()
     {
