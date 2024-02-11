0deb9dcb465ac320415ccea5c828fa34c2a1ae89
pinyi <s110919041@stu.ntue.edu.tw>
2024-02-11 23:41:22 +0800

    毒蛇謎題測試未完成

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,136 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class MusicManager : MonoBehaviour
+{
+    public GameObject[] musics;
+    private int currentIndex = 0;
+    private Animator anim;
+    private Animator animMusic1;
+    private static bool musicCorrect;
+    private int clickCount = 0;
+    private bool animationInitialized = false;
+    private List<GameObject> clickedMusics = new List<GameObject>();
+
+    public GameObject talkUI;
+
+    private void Start()
+    {
+        SetNextMusic();
+        anim = GetComponent<Animator>();
+        animMusic1 = GetComponent<Animator>();
+        anim.SetInteger("CandleCorrect", 0);
+    }
+
+    void Update()
+    {
+        // 如果你希望每帧都更新动画状态，可以在这里调用 UpdateAnimationState()
+    }
+
+    public void MusicClicked(GameObject clickedMusic)
+    {
+        if (!clickedMusics.Contains(clickedMusic))
+        {
+            clickedMusics.Add(clickedMusic);
+
+            if (clickedMusic == musics[currentIndex])
+            {
+                // 正確的音樂
+                PlayMusicAnimation(clickedMusic, currentIndex);
+                currentIndex++;
+                clickCount++;
+
+                if (currentIndex == musics.Length)
+                {
+                    // 完成整個順序
+                    Debug.Log("完成順序！");
+                    ResetMusicSequence();
+                    musicCorrect = true;
+                }
+                else
+                {
+                    // 設置下一個應點擊的音樂
+                    SetNextMusic();
+                }
+            }
+            else
+            {
+                // 錯誤的音樂，可以在這裡添加錯誤處理邏輯
+                Debug.Log("錯誤的順序！");
+                musicCorrect = false;
+                ResetMusicSequence();
+                clickCount++;
+            }
+
+            // 更新動畫狀態
+            if (clickCount == 3) UpdateAnimationState();
+        }
+        else
+        {
+            // 已經點過這個音樂，你可以添加相應的處理邏輯，例如提示或其他操作
+            Debug.Log("這個音樂已經點過了！");
+        }
+    }
+
+    private void PlayMusicAnimation(GameObject music, int index)
+    {
+        // 在這裡添加播放動畫的邏輯
+        switch (index)
+        {
+            case 0:
+                Debug.Log("點到音樂1");
+                // 播放第一个音樂的动画
+                animMusic1.SetInteger("music1Anim", 1);
+                break;
+            case 1:
+                // 播放第二个音樂的动画
+                // 添加你的第二个音乐的动画播放逻辑
+                break;
+            // ... 继续添加其他音乐的情况
+        }
+    }
+
+    private void SetNextMusic()
+    {
+        // 在這裡添加設置下一個音樂的邏輯
+    }
+
+    private void ResetMusicSequence()
+    {
+        currentIndex = 0;
+    }
+
+    private void UpdateAnimationState()
+    {
+        // 根據musicCorrect狀態設置動畫
+        if (musicCorrect)
+        {
+            anim.SetInteger("CandleCorrect", 1);
+            float delayAnim = 3.0f;
+            Invoke("showAnim", delayAnim);
+             Debug.Log("成功到一");
+        }
+        else
+        {
+            anim.SetInteger("CandleCorrect", 2);
+            float delayInSeconds = 4.0f;
+            Invoke("LoadNextScene", delayInSeconds);
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void showAnim()
+    {
+        talkUI.SetActive(true);
+    }
+
+    public static bool UpdateMusicCorrect()
+    {
+        return musicCorrect;
+    }
+}
---
+++ Assets/Musicbtn.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Musicbtn : MonoBehaviour, IPointerDownHandler
+{
+    public MusicManager musicManager; // 连接到MusicManager脚本
+    private Animator animMusic;
+    public int musicNumber;
+
+    private void Start()
+    {
+        animMusic = GetComponent<Animator>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown called");
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPoint);
+
+        if (musicNumber == 1)
+        {
+            animMusic.SetInteger("music1Anim", 1);
+            Debug.Log("Animator component: " + animMusic); // 检查获取到的 Animator 组件
+            Debug.Log("设置 music1Anim 为 1");
+        }
+
+        if (musicNumber == 2)
+        {
+            animMusic.SetInteger("music1Anim", 1);
+        }
+
+        // animMusic.SetInteger("animMusic2", 1);
+        // 当音乐被点击时调用MusicManager的MusicClicked函数
+        musicManager.MusicClicked(gameObject);
+
+        if (musicNumber == 3)
+        {
+            animMusic.SetInteger("music1Anim", 1);
+        }
+
+        if (musicNumber == 4)
+        {
+            animMusic.SetInteger("music1Anim", 1);
+        }
+    }
+}
